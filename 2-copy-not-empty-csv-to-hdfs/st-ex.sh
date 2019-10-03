9afebd15e30f3b760c8f7425df0c6034d14e1167
Anton Lastauka <a.lastauka@gmail.com>
2019-10-03 08:41:56 +0200

    updated script & enriched README for hive writing

--- 2-copy-not-empty-csv-to-hdfs/st-ex.sh
+++ 2-copy-not-empty-csv-to-hdfs/st-ex.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --name hadoop --hostname=quickstart.cloudera --privileged=true -td -p 2181:2181 -p 8888:8888 -p 7180:7180 -p 80:80 -p 8020:8020 -p 9000:9000 -p 50070:50070 cloudera/quickstart /usr/bin/docker-quickstart
+sudo docker run --name hadoop --hostname=quickstart.cloudera --privileged=true -td -p 2181:2181 -p 9083:9083 -p 9043:9043 -p 8888:8888 -p 7180:7180 -p 80:80 -p 8020:8020 -p 9000:9000 -p 50070:50070 cloudera/quickstart /usr/bin/docker-quickstart
